eef109d669122453bc130c993dbd584893bb0fb4
trexlerdevin <trexlerdevin@gmail.com>
2021-06-24 13:29:09 -0700

    corrected modal errors

--- Trexler_Cafe/js/main.js
+++ Trexler_Cafe/js/main.js
@@ -1,24 +1,40 @@
-//enlarge image / open modal
-var modal = document.getElementsByName("photo");
-
-// button to open the modal
-var btn = document.getElementById("btn");
-
-// the <span> element that closes the modal
-var span = document.getElementsByClassName("close");
-
-// open modal when clicked
-btn.onclick = function openModal() {
-    modal.style.display = "block"
-}
-
-// close the modal when the <span> (x) is clicked
-span.onclick = function() {
-        modal.style.display = "none"
-}
-
-window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none"
-        }
-}
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal([]) {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
